cfb585e52dca2432c6382dab7afc9ce0ccce534e
aytang <albertyptang@gmail.com>
2015-09-05 16:31:27 -0700

    LongestWord

--- src/FirstReverse.java
+++ src/FirstReverse.java
@@ -2,22 +2,23 @@ import java.util.Scanner;
 import java.io.*;
 
 class FirstReverse {  
-  String firstReverse(String str) { 
-  
+  String firstReverse(String str) {
+
     // code goes here   
-    String reversedString = "";
-	for (int i = str.length() - 1; i >= 0; i--) {
-		reversedString += str.charAt(i);
-	}
-    return reversedString;    
-  } 
-  /* Note: In Java the return type of a function and the 
+    /* Note: In Java the return type of a function and the 
        parameter types being passed are defined, so this return 
        call must match the return type of the function.
        You are free to modify the return type. */
+    String reversedString = "";
+    for (int i = str.length() - 1; i >= 0; i--) {
+      reversedString += str.charAt(i);
+      }
+    return reversedString;    
+    
+  } 
   
   public static void main (String[] args) {  
-    // keep this function call here     
+    // keep this function call here
     Scanner s = new Scanner(System.in);
     FirstReverse c = new FirstReverse();
     System.out.print(c.firstReverse(s.nextLine())); 
--- src/LongestWord.java
+++ src/LongestWord.java
@@ -1,28 +1,32 @@
 import java.util.Scanner;
 import java.io.*;
 
-class FirstFactorial {  
-  int firstFactorial(int num) { 
+class LongestWord {  
+  String longestWord(String sen) { 
   
     // code goes here   
     /* Note: In Java the return type of a function and the 
        parameter types being passed are defined, so this return 
        call must match the return type of the function.
        You are free to modify the return type. */
-    int factorial = 1;
-    while(num > 0){
-      factorial *= num;
-      num--;
-      }
-    return factorial;    
+    String[] words = sen.split(" ");
+    int length = 0;
+    String longestWord = "";
+    for (String word : words) {
+      if (word.length() > length) {
+        longestWord = word;
+        length = word.length();
+      }    
+    }
+    return longestWord;    
   
   }
   
   public static void main (String[] args) {  
     // keep this function call here     
     Scanner s = new Scanner(System.in);
-    FirstFactorial c = new FirstFactorial();
-    System.out.print(c.firstFactorial(s.nextInt())); 
+    LongestWord c = new LongestWord();
+    System.out.print(c.longestWord(s.nextLine())); 
     s.close();
   }   
   
